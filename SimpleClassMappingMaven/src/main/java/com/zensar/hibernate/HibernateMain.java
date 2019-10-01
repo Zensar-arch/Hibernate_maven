69cf0aa9002523c4cc573519497f5db0ee64392b
Susmita Basak <susum05@gmail.com>
2019-10-01 11:35:24 +0530

    Initial commit added simple class mapping maven project.

---
+++ SimpleClassMappingMaven/src/main/java/com/zensar/hibernate/HibernateMain.java
@@ -0,0 +1,36 @@
+package com.zensar.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.google.protobuf.AbstractMessage.Builder;
+import com.zensar.hibernate.entities.Product;
+
+/**
+ * Hello world!
+ *
+ */
+public class HibernateMain 
+{
+    public static void main( String[] args )
+    {
+    	Configuration c = new Configuration().configure();
+    	SessionFactory s = c.buildSessionFactory();
+    	Session s1 = s.openSession();
+    	Transaction t = s1.beginTransaction();
+    	
+    	Product p = new Product();
+    	p.setProductId(1008);
+    	p.setName("Iron");
+    	p.setBrand("Koryo");
+    	p.setPrice(899);
+    	s1.save(p);
+    	
+    	
+    	t.commit();
+    	s1.close();
+        //System.out.println( "Hello World!" );
+    }
+}
